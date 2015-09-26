675504e86bcf8cfefdf0fe8d64b5f1b9111f99f4
NWChen <neilwchen@gmail.com>
2015-09-25 20:56:43 -0400

    image file upload works

--- Fire.js
+++ Fire.js
@@ -1,3 +1,39 @@
+var links = new FS.Store.GridFS("links");
+
+Links = new FS.Collection("links", {
+  stores: [new FS.Store.FileSystem("images", {path: ".uploads"})]
+});
+
+Links.deny({
+  insert: function(){
+    return false;
+  },
+  update: function(){
+    return false;
+  },
+  remove: function(){
+    return false;
+  },
+  download: function(){
+    return false;
+  }
+});
+
+Links.allow({
+ insert: function(){
+  return true;
+ },
+ update: function(){
+  return true;
+ },
+ remove: function(){
+  return true;
+ },
+ download: function(){
+  return true;
+ }
+});
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
@@ -5,6 +41,9 @@ if (Meteor.isClient) {
   Template.hello.helpers({
     counter: function () {
       return Session.get('counter');
+    },
+    images: function () {
+      return Links.find();
     }
   });
 
@@ -12,12 +51,23 @@ if (Meteor.isClient) {
     'click button': function () {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
+    },
+    'change .myFileInput': function(event, template) {
+      FS.Utility.eachFile(event, function(file) {
+        Links.insert(file, function (err, fileObj) {
+          if (err) {
+            console.log("ERROR: ", err);
+          } else {
+            console.log("SUCCESSFUL UPLOAD");
+          }
+        });
+      });
     }
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    
   });
 }
