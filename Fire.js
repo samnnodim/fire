d2866ceba173af04a9ebe5b30ccc721a9722fe28
NWChen <neilwchen@gmail.com>
2015-10-02 16:04:39 -0400

    routing

--- Fire.js
+++ Fire.js
@@ -1,89 +1,16 @@
-var links = new FS.Store.GridFS("links");
-
-Links = new FS.Collection("links", {
-  stores: [new FS.Store.FileSystem("images", {path: ".uploads"})]
-});
-
-Links.deny({
-  insert: function(){
-    return false;
-  },
-  update: function(){
-    return false;
-  },
-  remove: function(){
-    return false;
-  },
-  download: function(){
-    return false;
-  }
-});
-
-Links.allow({
- insert: function(){
-  return true;
- },
- update: function(){
-  return true;
- },
- remove: function(){
-  return true;
- },
- download: function(){
-  return true;
- }
-});
-
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    },
-    images: function () {
-      return Links.find();
+  Template.form.events({
+    'click .form-button': function(event) {
+      event.preventDefault();
+      window.location.href="/link";
     }
-  });
-
-  Template.home.helpers({
-    link: function () {
-      return Session.get('link');
-    }
-  });
+  })
 
   Template.loading.helpers({
     status: function () {
       return Session.get('status');
     }
   })
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    },
-    'change .myFileInput': function(event, template) {
-      FS.Utility.eachFile(event, function(file) {
-        Links.insert(file, function (err, fileObj) {
-          if (err) {
-            console.log("ERROR: ", err);
-          } else {
-            console.log("SUCCESSFUL UPLOAD");
-          }
-        });
-      });
-    }
-  });
-
-  Template.form.events({
-    'click .form-button': function(event) {
-      event.preventDefault();
-      Session.set('link', true);
-      console.log("Mixtape detected");
-    }
-  });
 
   Template.loading.rendered = function() {
     if(!this._rendered) {
@@ -101,7 +28,15 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
+  Meteor.startup(function() {
     
   });
 }
+
+Router.route('/', function() {
+  this.render('form');
+});
+
+Router.route('/link', function() {
+  this.render('loading');
+});
