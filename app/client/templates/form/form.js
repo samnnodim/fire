8918dc7f2acd81301169347b98705851c47c41ba
NWChen <neilwchen@gmail.com>
2015-11-06 16:18:09 -0500

    YouTube player embedded. If isitfire.com is entered, send default result

--- app/client/templates/form/form.js
+++ app/client/templates/form/form.js
@@ -3,23 +3,40 @@
 /*****************************************************************************/
 
 var lsContext;
+var url;
 
 Template.Form.events({
   'submit #form-submit' : function(event) {
-    //validate that submitted text is a URL
-    var url = event.target.link.value;
-    var domain = "";
-    if (url.indexOf("://") > -1) {
-      domain = url.split('/')[2];
-    } else {
-        domain = url.split('/')[0];
-    }
-    domain = domain.split(':')[0];
+    // Stop default event action
+    event.preventDefault();
 
+    // Save the url entered
+    var url = String(event.target.text.value);
+    Session.setPersistent('url', url);
+
+    // Clean up submitted text
+    var domain = "";
+    if (url.indexOf("://") > -1)
+      domain = url.split('/')[2];
+    else 
+      domain = url.split('/')[0];
+    domain = domain.split(':')[
+    0];
+
+    // Check if submitted text is a valid URL
     if(lsContext.validate({"url": domain})) {
-      $('.invalid-warning').css("visibility", "nhidden");
-      Session.set("link", true);
+      $('.invalid-warning').css("visibility", "hidden");
+      Session.set('link', true);
+
+      // YouTube API call
+      var requestUrl = "https://www.googleapis.com/youtube/v3/videos?id="
+        + url.substring(Session.get('url').indexOf("v=") + 2) + "&key=" + YOUTUBE_API_KEY + "&part=snippet&callback=?";
+      $.getJSON(requestUrl, function (data) {
+        Session.setPersistent("data", data)
+      });
     } else {
+
+      // If not a valid URL, tell the user
       $('.invalid-warning').css("visibility", "visible");
     }
   }
--- app/client/templates/result/result.js
+++ app/client/templates/result/result.js
@@ -9,8 +9,15 @@ Template.Result.events({
 /*****************************************************************************/
 Template.Result.helpers({
   state: function () {
+
+  	// Levels of fire
     var heat_levels = ["SMOKEY BEAR-LEVEL SHIT. IT IS FIRE.", "QUIETLY BOILING WATER, MAYBE NEXT TIME", "ICE COLD BULLSHIT, TAKE THAT BACK WHERE YOU FOUND IT"];
-    return heat_levels[Math.floor(Math.random()*heat_levels.length)];
+
+    // isitfire.com is fire af
+    if (Session.get('url').indexOf("isitfire.com") > -1)
+    	return "FOREST FIRE SHIT. CAN YOU FETCH A BROTHER SOME WATER?";
+    else 
+    	return heat_levels[Math.floor(Math.random()*heat_levels.length)];
   }
 });
 
@@ -18,6 +25,9 @@ Template.Result.helpers({
 /* Result: Lifecycle Hooks */
 /*****************************************************************************/
 Template.Result.onCreated(function () {
+	console.log("https://www.googleapis.com/youtube/v3/videos?id="
+        + Session.get("url").substring(Session.get('url').indexOf("v=") + 2) + "&key=" + YOUTUBE_API_KEY + "&part=snippet&callback=?");
+	console.log(Session.get("data"));
 });
 
 Template.Result.onRendered(function () {
